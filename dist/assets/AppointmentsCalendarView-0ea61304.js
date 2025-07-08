bcbc7e36c3758ae79932fc0a8399c413409e8984
Iván García <garciayalaivan@gmail.com>
2025-07-08 13:44:48 +0200

    BACKEND - Visualización del Perfil

--- dist/assets/AppointmentsCalendarView-0ea61304.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-7f476d33.js";const t=()=>e.jsx("div",{children:e.jsx("h2",{children:"Calendario de Citas"})});export{t as default};
---
+++ dist/assets/AppointmentsCalendarView-87e227b7.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-75f81b91.js";const t=()=>e.jsx("div",{children:e.jsx("h2",{children:"Calendario de Citas"})});export{t as default};
